f3f771f45610aefd02f9d516b81ee814555265eb
‘Juliano <julianoperins@gmail.com>
2020-02-09 23:54:13 -0500

    third commit

--- script.js
+++ script.js
@@ -3,11 +3,18 @@ const hours = document.getElementById('hours');
 const minutes = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
 const countdown = document.getElementById('countdown');
+const year = document.getElementById('year');
+const loading = document.getElementById('loading');
+
 
 const currentYear = new Date().getFullYear();
 
 const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
 
+// Set background year
+year.innerText = currentYear + 1;
+
+// Update time
 function updateCountdown() {
     const currentTime = new Date();
     const diff = newYearTime - currentTime;
@@ -24,5 +31,14 @@ function updateCountdown() {
     seconds.innerHTML = s < 10 ? '0' + s : s;
     console.log(s);
 }
+
+//Show spinner before countdown
+setTimeout(() => {
+    loading.remove();
+    countdown.style.display = 'flex';
+}, 1000);
+
+
+// run every second
 setInterval(updateCountdown, 1000);
-updateCountdown();
+
